fef7f96114990d3058c79bac6750d6ff77621190
GreyStinger <jaydenandrews010@gmail.com>
2024-10-16 14:20:44 +0200

    re-added gap to monacle and single window view

--- config.def.h
+++ config.def.h
@@ -6,35 +6,42 @@
 static const unsigned int borderpx  = 4;        /* border pixel of windows */
 static const unsigned int gappx     = 2;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+
+/* dwmbar */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = {
+
+/* fonts */
+static const char *fonts[] = {
 	"JetBrainsMono Nerd Font:size=11:antialias=true",
-	"monospace:size=10",
 };
-static const char dmenufont[]       = "JetBrainsMono Nerd Font:size=12";
+static const char dmenufont[]       = "JetBrainsMono Nerd Font:size=13";
+
+/* colors */
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
+static const char col_blue_gray[]   = "#387478";
 static const char col_dp[]          = "#460C68";
 static const char col_lp[]          = "#CB1C8D";
-static const unsigned int baralpha = 0xd0;
-static const unsigned int borderalpha = OPAQUE;
+static const unsigned int baralpha_sel = 0x70;
+static const unsigned int baralpha = 0x00;
+static const unsigned int borderalpha = 0x00;
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_dp, col_lp },
+	[SchemeSel]  = { col_gray4, col_blue_gray, col_lp },
 };
 static const unsigned int alphas[][3]      = {
-    /*               fg      bg        border*/
-	[SchemeNorm] = { OPAQUE, baralpha, borderalpha },
-	[SchemeSel]  = { OPAQUE, baralpha, borderalpha },
+	/*               fg      bg           	border*/
+	[SchemeNorm] = { OPAQUE, baralpha,    	borderalpha },
+	[SchemeSel]  = { OPAQUE, baralpha_sel,	borderalpha },
 };
 
 /* tagging */
-static const char *tags[] = { " 󰅬", " 󰈹", " 󰙯", " 󰩹", " 󰗅" };
+static const char *tags[] = { "󰅬", "󰈹", "󰙯", "4", "5", "6", "7", "8", "󰗅" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -68,7 +75,7 @@ static const Layout layouts[] = {
 	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
-#define SHCMD(cmd) { .v = (const char*[]){ "/usr/bin/env zsh", "-c", cmd, NULL } }
+#define SHCMD(cmd) { .v = (const char*[]){ "/usr/bin/env", "zsh", "-c", cmd, NULL } }
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
@@ -78,11 +85,12 @@ static const char *dmenucmd[] = {
 	"-fn", dmenufont,
 	"-nb", col_gray1,
 	"-nf", col_gray3,
-	"-sb", col_cyan,
+	"-sb", col_blue_gray,
 	"-sf", col_gray4,
 	NULL
 };
-static const char *termcmd[]  = { "/usr/bin/env zsh", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
+static const char *screenshotcmd[]  = { "flameshot", "gui", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -96,7 +104,8 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
+	{ 0,                            0x0000ff61,spawn,          {.v = screenshotcmd } },
+	{ MODKEY,             XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY,                       XK_q,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
@@ -115,10 +124,10 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_3,                      2)
 	TAGKEYS(                        XK_4,                      3)
 	TAGKEYS(                        XK_5,                      4)
-	/*TAGKEYS(                        XK_6,                      5)*/
-	/*TAGKEYS(                        XK_7,                      6)*/
-	/*TAGKEYS(                        XK_8,                      7)*/
-	/*TAGKEYS(                        XK_9,                      8)*/
+	TAGKEYS(                        XK_6,                      5)
+	TAGKEYS(                        XK_7,                      6)
+	TAGKEYS(                        XK_8,                      7)
+	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 };
 
